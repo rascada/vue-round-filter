e16b3af66917557bc85cfc7eb65f7f1d68268ae7
rascada <carftjr@gmail.com>
2016-01-19 17:38:29 +0100

    refactor

--- index.js
+++ index.js
@@ -3,9 +3,5 @@ module.exports = function round(value, accuracy, keep) {
 
   let fixed = value.toFixed(accuracy);
 
-  while (!keep && !+fixed.slice(-1)) {
-    fixed = fixed.slice(0, -1);
-  }
-
-  return value;
+  return keep ? fixed : +fixed;
 };
